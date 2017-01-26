dd451601d1f583b85e6c8ca2d778f35302076719
Harrmalik <harrmalik@gmail.com>
2017-01-26 15:46:18 -0500

    base project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+$(document).ready(function(){
+	//Javascript variables
+	var date = moment.format('MMM Do');
+	var time = '';
 
+	// HTML Buttons
+	var checkInBtn = $('#checkIn');
+	var checkOutBtn = $('#checkOut');
+	var timesheetBtn = $('#timesheet');
+	$('#today').html(date);
+
+	// Event Listeners
+	checkInBtn.on("click", function() {
+		time = moment.format('HH:MM:SS');
+		updateToday(time, '#timeIn');
+		updateTimesheet();
+	});
+
+	checkOutBtn.on("click", function() {
+		time = moment.format('HH:MM:SS');
+		updateToday(time, '#timeOut');
+		updateTimesheet();
+	});
+
+	timesheetBtn.on("click", function() {
+		window.location('/timesheet.php');
+	});
+
+	// Functions
+	var updateToday = function(time, field) {
+		$(field).html(time);
+	};
+
+	var updateTimesheet = function() {
+
+	};
+}
