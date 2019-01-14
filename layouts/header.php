444c50e6ca80e938fb21d8c5c1bff169b3b2ac3e
Malik Harrison <harrmalik@gmail.com>
2019-01-14 13:38:00 -0500

    moved files

--- layouts/header.php
+++ layouts/header.php
@@ -28,11 +28,7 @@
 
 		<link rel="stylesheet" href="<?php echo $assets; ?>css/main.css"/>
 		<link rel="stylesheet" href="css/styles.css"/>
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/all.css">
 
 		<?php include_once($assets . "js/scripts.php"); ?>
 		<script src="js/moment-timezone.js"></script>
-
-        <!--[if IE]>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
